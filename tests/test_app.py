405d1610efb2a52d05edfe8313f26b9c41725b07
Tomislav Dukez <tomdu3@ymail.com>
2025-02-01 20:27:45 +0000

    add user creation test

---
+++ tests/test_app.py
@@ -0,0 +1,28 @@
+import pytest
+from shop_app.app import UserType, User
+
+# Test data
+USERNAME = "test_user"
+PASSWORD = "test_password"
+ADMIN_USERNAME = "admin_user"
+ADMIN_PASSWORD = "admin_password"
+
+# regular_user
+@pytest.fixture
+def regular_user():
+    return User(USERNAME, PASSWORD, UserType.user)
+
+# admin_user
+@pytest.fixture
+def admin_user():   
+    return User(ADMIN_USERNAME, ADMIN_PASSWORD, UserType.admin)
+
+# Test cases for UserType and User class
+def test_user_initialization(regular_user):
+    """
+    Test correct creation of a User object.
+    """
+    assert regular_user.username == USERNAME
+    assert regular_user.password == PASSWORD
+    assert regular_user.user_type == UserType.user
+    assert regular_user.session_id is None
